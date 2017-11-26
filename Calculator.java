2300d89990871af5238b4fcadaba95c4b924ea3a
irina1619 <34001026+irina1619@users.noreply.github.com>
2017-11-26 15:16:35 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please input first number:");
+        double firstNum = in.nextDouble();
+        System.out.println("Please input second number:");
+        double secondNum  = in.nextDouble();
+        while (secondNum == 0){
+            System.out.println("The second number can't be zero. Please, enter nonzero number ");
+            secondNum  = in.nextDouble();
+        }
+        System.out.println("Sum is " + (firstNum + secondNum));
+        System.out.println("Difference is " + (firstNum - secondNum));
+        System.out.println("Multiplication is " + (firstNum * secondNum));
+        System.out.println("Division is " + (firstNum / secondNum));
+
+    }
+}

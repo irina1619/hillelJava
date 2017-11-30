9b2363631bbc9c069bbce4a5c9c0bffac0c20778
irina1619 <34001026+irina1619@users.noreply.github.com>
2017-11-30 14:07:02 +0200

    Обновленный калькулятор

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,65 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please input first number:");
-        double firstNum = in.nextDouble();
-        System.out.println("Please input second number:");
-        double secondNum  = in.nextDouble();
-        while (secondNum == 0){
-            System.out.println("The second number can't be zero. Please, enter nonzero number ");
-            secondNum  = in.nextDouble();
-        }
-        System.out.println("Sum is " + (firstNum + secondNum));
-        System.out.println("Difference is " + (firstNum - secondNum));
-        System.out.println("Multiplication is " + (firstNum * secondNum));
-        System.out.println("Division is " + (firstNum / secondNum));
+
+        double firstNum = getDouble();
+        double secondNum = getDouble();
+        char operation = getOperation();
+        double result = calc(firstNum,secondNum,operation);
+        System.out.println("The result is "+result);
 
     }
+
+    public static double getDouble(){
+        System.out.println("Please input a number:");
+        double num;
+        if (scanner.hasNextDouble()){
+            num = scanner.nextDouble();
+        } else{
+            System.out.println("You made a mistake. Enter a digit:");
+            scanner.next();
+            num = getDouble();
+        }
+        return num;
+
+    }
+    public static char getOperation(){
+        System.out.println("Enter the operation");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("You made a mistake. Please, try again");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static double calc(double firstNum, double secondNum, char operation){
+        double result;
+        switch (operation){
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                result = firstNum / secondNum;
+                break;
+            default:
+                System.out.println("Try again:");
+                result = calc(firstNum, secondNum, getOperation());
+        }
+        return result;
+    }
+
+
 }
